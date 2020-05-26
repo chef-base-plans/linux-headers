159b1fd81c846dbc2d3d3f2e82480f2d9b2362a0
Steven Marshall <SMarshall@chef.io>
2020-05-26 15:08:38 +0100

    [linux-headers] Switching back to core origin
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/linux-headers_exists.rb
+++ controls/linux-headers_exists.rb
@@ -1,5 +1,5 @@
 plan_dirs = input("plan_dirs")
-plan_name = input("plan_name", value: "mindnumbing/linux-headers")
+plan_name = input("plan_name", value: "core/linux-headers")
 base_dir = input("base_dir", value: "include")
 
 hab_pkg_path = command("hab pkg path #{plan_name}")
