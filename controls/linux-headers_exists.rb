edfb97c1dbff451b8f80ec1d9f41f08bca54ff58
Steven Marshall <SMarshall@chef.io>
2020-05-26 14:50:09 +0100

    [linux-headers] Update contact emails and refactor inspec testing
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/linux-headers_exists.rb
+++ controls/linux-headers_exists.rb
@@ -1,13 +1,19 @@
-plan_name = input("plan_name", value: "core/linux-headers")
+plan_dirs = input("plan_dirs")
+plan_name = input("plan_name", value: "mindnumbing/linux-headers")
+base_dir = input("base_dir", value: "include")
 
-plan_path = command("hab pkg path #{plan_name}").stdout.strip
-plan_dir = input("plan_dir", value: "/include/")
-target_dir = "#{plan_path}#{plan_dir}"
+hab_pkg_path = command("hab pkg path #{plan_name}")
+describe hab_pkg_path do
+  its('exit_status') { should eq 0 }
+  its('stdout') { should_not be_empty }
+end
 
-plan_files = input("plan_files")
+target_dir = File.join(hab_pkg_path.stdout.strip, base_dir)
 
-plan_files.each do | file |
-  describe file("#{target_dir}#{file}") do
-    it("#{file} should exist") { should exist }
+plan_dirs.each do | plan_dir |
+  describe command("ls -al #{File.join(target_dir, plan_dir)}") do
+    its('stdout') { should_not be_empty }
+    its('stderr') { should eq '' }
+    its('exit_status') { should eq 0 }
   end
 end
