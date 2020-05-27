f5a473673be7ae1d2044ee21ef064ea71d2ae220
Steven Marshall <SMarshall@chef.io>
2020-05-27 13:29:26 +0100

    [linux-headers] Pull origin from ENV
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/linux-headers_exists.rb
+++ controls/linux-headers_exists.rb
@@ -1,8 +1,10 @@
-plan_dirs = input("plan_dirs")
-plan_name = input("plan_name", value: "core/linux-headers")
 base_dir = input("base_dir", value: "include")
+plan_dirs = input("plan_dirs")
+plan_origin = ENV['HAB_ORIGIN']
+plan_name = input("plan_name", value: "core/linux-headers")
+plan_ident = "#{plan_origin}/#{plan_name}"
 
-hab_pkg_path = command("hab pkg path #{plan_name}")
+hab_pkg_path = command("hab pkg path #{plan_ident}")
 describe hab_pkg_path do
   its('exit_status') { should eq 0 }
   its('stdout') { should_not be_empty }
