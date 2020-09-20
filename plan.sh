fa34a3746c5cdad433fcd0c84ccf28312a41600f
Steven Marshall <SMarshall@chef.io>
2020-09-20 20:51:04 +0100

    Revert "[linux-headers] Update to 4.20.17"
    This reverts commit 60cc1d1727f76d91b633ea056d42a97bb352d30e.

--- plan.sh
+++ plan.sh
@@ -1,12 +1,12 @@
 pkg_name=linux-headers
 pkg_origin=core
-pkg_version=4.20.17
+pkg_version=4.19.62
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The Linux kernel headers"
 pkg_upstream_url="https://kernel.org"
 pkg_license=('GPL-2.0-or-later')
 pkg_source="https://www.kernel.org/pub/linux/kernel/v4.x/linux-${pkg_version}.tar.xz"
-pkg_shasum="d011245629b980d4c15febf080b54804aaf215167b514a3577feddb2495f8a3e"
+pkg_shasum="07be647189ced7eb8ba5ee769906e67919975772184842cc517f609df50cdadc"
 pkg_dirname="linux-$pkg_version"
 pkg_deps=()
 pkg_build_deps=(
