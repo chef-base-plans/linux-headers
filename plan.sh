d02641a79bc3f99640c3ef0672d94529e1d01b6d
Nimit <nimit.jyotiana@hotmail.com>
2022-01-07 11:03:19 +0530

    Revert "upgraded package version"

--- plan.sh
+++ plan.sh
@@ -1,12 +1,12 @@
 pkg_name=linux-headers
 pkg_origin=core
-pkg_version=4.19.213
+pkg_version=4.20.17
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="The Linux kernel headers"
 pkg_upstream_url="https://kernel.org"
 pkg_license=('GPL-2.0-or-later')
 pkg_source="https://www.kernel.org/pub/linux/kernel/v4.x/linux-${pkg_version}.tar.xz"
-pkg_shasum=38f28c1cd6db0b54ee37a14fd17db651fe68704d08f95e141b14d32b872f4e98
+pkg_shasum="d011245629b980d4c15febf080b54804aaf215167b514a3577feddb2495f8a3e"
 pkg_dirname="linux-$pkg_version"
 pkg_deps=()
 pkg_build_deps=(
